5f4b634f5873307062dd46f09fe458c339f0d5d4
monkeys1ne <mcdoob@gmail.com>
2022-12-30 10:04:12 +1300

    Post new error capture and display

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,15 +5,21 @@ class PostsController < ApplicationController
     end
 
     def new
+        @post = Post.new
     end
 
     def create
-        @post = Post.new(post_params)
-        -if @post.save
-            redirect_to @post
-        else
-            render 'new'
-        end
+        respond_to do |format|
+            @post = Post.new(post_params)
+            if @post.save
+              format.html { redirect_to @post }
+            else
+              # NOTE: this will render `new.erb` and set 
+              #       `Content-Type: text/html` header;
+              #       turbo is happy.
+              format.html { render(:new, status: :unprocessable_entity) }
+            end
+          end
     end
 
     def show
